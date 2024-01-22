46a50734fc6765fdc583ad50fd72974f71ea8997
leonardo <leonardocasa790@gmail.com>
2024-01-22 00:25:37 -0300

    chore(): create fn for read path

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
 module Main where
-
-import Lexing.Lexer (getModuleName)
--- import qualified Parsing.Paser as p
--- import qualified Rendering.Render as ren
+import System.IO
 
 main :: IO ()
-main =  do putStrLn $ "Hello world" ++ getModuleName
+main =  do
+    print ""
+
+    -- read path for file
     -- putStrLn $ "Hello world" ++ par.getModuleName
     -- putStrLn $ "Hello world" ++ ren.getModuleName
+
+
+
+
+
+readContent :: FilePath -> IO String
+readContent filePath = do
+    handle <- openFile filePath ReadMode
+    content <- hGetContents handle
+    hClose handle
+    return content
