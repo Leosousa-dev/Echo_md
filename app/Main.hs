52b4568864d96d883ff80fa111dbe7f51bfa7d26
leonardo <leonardocasa790@gmail.com>
2024-01-22 00:42:57 -0300

    chore(): removed comments

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,5 @@ main =  do
     content <- readContent "app/Main.hs"
     putStrLn content
 
-    -- read path for file
-    -- putStrLn $ "Hello world" ++ par.getModuleName
-    -- putStrLn $ "Hello world" ++ ren.getModuleName
-
-
-
 
 
